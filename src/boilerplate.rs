49ea12554baf5558175d1647e74701b6a24edae1
IDEDARY <IDEDARY@gmail.com>
2024-02-17 15:24:44 +0100

    sync

--- src/boilerplate.rs
+++ src/boilerplate.rs
@@ -22,6 +22,7 @@ pub struct MenuUi;
 // Load all assets at startup for faster loading during runtime
 #[derive(Resource)]
 pub struct AssetCache {
+    pub music: Handle<AudioSource>,
     pub font: Handle<Font>,
     pub font_bold: Handle<Font>,
     pub button: Handle<Image>,
@@ -36,6 +37,7 @@ pub struct AssetCache {
 }
 pub fn presetup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.insert_resource(AssetCache {
+        music: asset_server.load("sounds/main_menu.ogg"),
         font: asset_server.load("fonts/rajdhani/Rajdhani-Medium.ttf"),
         font_bold: asset_server.load("fonts/rajdhani/Rajdhani-Bold.ttf"),
         button: asset_server.load("images/main_menu/button.png"),
@@ -44,8 +46,108 @@ pub fn presetup(mut commands: Commands, asset_server: Res<AssetServer>) {
         switch_head: asset_server.load("images/settings/switch_head.png"),
 
         main_background: asset_server.load("images/main_menu/background.png"),
-        main_board: asset_server.load("images/main_menu/board.png"),
+        main_board: asset_server.load("images/main_menu/board1.png"),
         main_logo: asset_server.load("images/main_menu/bevypunk.png"),
         settings_background: asset_server.load("images/settings/background.png"),
     });
+}
+
+
+
+
+
+
+
+
+use std::f32::consts::TAU;
+use bevy::core_pipeline::bloom::{BloomSettings, BloomPrefilterSettings, BloomCompositeMode};
+use bevy::core_pipeline::tonemapping::Tonemapping;
+use rand::Rng;
+
+
+/// Spawns camera will all custom settings
+pub fn camera() -> impl Bundle {
+    (
+        MenuUi,
+        Camera2dBundle {
+            transform: Transform::from_xyz(0.0, 0.0, 1000.0),
+            camera: Camera {
+                hdr: true,
+                ..default()
+            },
+            tonemapping: Tonemapping::None,
+            ..default()
+        },
+        BloomSettings {
+            intensity: 0.20,
+            low_frequency_boost: 0.8,
+            low_frequency_boost_curvature: 0.95,
+            high_pass_frequency: 0.9,
+            prefilter_settings: BloomPrefilterSettings {
+                threshold: 0.25,
+                threshold_softness: 0.1,
+            },
+            composite_mode: BloomCompositeMode::Additive,
+        },
+        /*VfxWiggleCamera {
+            sinusoid: vec![
+                Sine {
+                    speed: 0.005,
+                    amplitude: 0.003,
+                    degree: 0.0,
+                }
+            ]
+        }*/
+    )
+}
+
+
+/// System for smooth flickering of the camera's bloom
+fn vfx_bloom_flicker(mut query: Query<&mut BloomSettings>) {
+    for mut bloom in &mut query {
+        let mut rng = rand::thread_rng();
+        if rng.gen_range(0..100) < 20 {
+            bloom.intensity += (rng.gen_range(0.20..0.25)-bloom.intensity)/5.0;
+            bloom.prefilter_settings.threshold += (rng.gen_range(0.25..0.30)-bloom.prefilter_settings.threshold)/5.0;
+        }
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct Sine {
+    pub speed: f32,
+    pub amplitude: f32,
+    pub degree: f32,
+}
+impl Sine {
+    fn tick(&mut self) {
+        self.degree += self.speed; 
+        if self.degree >= TAU { self.degree -= TAU; }
+        if self.degree < 0.0 { self.degree += TAU; }
+    }
+    fn get_pure(&self) -> f32 {
+        self.degree.sin()*self.amplitude
+    }
+}
+
+#[derive(Component, Clone, Default)]
+pub(super) struct VfxWiggleCamera {
+    pub sinusoid: Vec<Sine>
+}
+fn vfx_camera_wiggle(mut query: Query<(&mut VfxWiggleCamera, &mut Transform)>) {
+    for (mut animation, mut transform) in &mut query {
+        for sine in &mut animation.sinusoid {
+            sine.tick()
+        }
+        transform.rotation.z = animation.sinusoid[0].get_pure();
+    }
+}
+
+
+/// Plugin with VFX systems for our menu
+pub struct VFXPlugin;
+impl Plugin for VFXPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, (vfx_bloom_flicker, vfx_camera_wiggle));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use boilerplate::*;
 fn main() {
     App::new()
         .add_plugins((default_plugins(), UiPlugin::<NoData, NoData, MenuUi>::new()))
-        .add_plugins(UiDebugPlugin::<NoData, NoData, MenuUi>::new())
+        //.add_plugins(UiDebugPlugin::<NoData, NoData, MenuUi>::new())
+        .add_plugins(VFXPlugin)
         .add_systems(PreStartup, presetup)
         .add_systems(Startup, setup)
         .run();
@@ -16,24 +17,27 @@ fn main() {
 fn setup(mut commands: Commands, assets: Res<AssetCache>) {
 
     // Spawn camera
-    commands.spawn(( MenuUi, Camera2dBundle { transform: Transform::from_xyz(0.0, 0.0, 1000.0), ..default() } ));
+    commands.spawn(camera());
+
+    // Spawn audio
+    commands.spawn( AudioBundle { source: assets.music.clone(), settings: PlaybackSettings::LOOP.with_volume(bevy::audio::Volume::new(0.5)) } );
 
     // Spawn the master ui
     commands.spawn((
         UiTreeBundle::<NoData, NoData, MenuUi>::from(UiTree::new("Bevypunk")),
         MovableByCamera,
-    )).with_children(|parent| {
+    )).with_children(|ui| {
 
         // Spawn the root div
         let root = UiLink::path("Root");
-        parent.spawn((
+        ui.spawn((
             MenuUi,
             root.clone(),
             UiLayout::Window::FULL.pack(),
         ));
 
         // Spawn the background
-        parent.spawn((
+        ui.spawn((
             MenuUi,
             root.add("Background"),
             UiLayout::Solid::new().size(Abs((1920.0, 1080.0))).cover(Cover::Full).pack(),
@@ -41,11 +45,25 @@ fn setup(mut commands: Commands, assets: Res<AssetCache>) {
         ));
 
         // Spawn the board
-        parent.spawn((
+        let board = root.add("Board");
+        ui.spawn((
             MenuUi,
-            root.add("Board"),
+            board.clone(),
             UiLayout::Solid::new().size(Abs((807.0, 1432.0))).align_x(Align(-0.8)).pack(),
             UiImage2dBundle::from(assets.main_board.clone())
+        ));
+
+        // Spawn the logo
+        ui.spawn((
+            MenuUi,
+            board.add("Boundary"),
+            UiLayout::Window::new().pos(Prc((-5.0, 12.0))).size(Prc((110.0, 20.0))).pack(),
+        ));
+        ui.spawn((
+            MenuUi,
+            board.add("Boundary/Logo"),
+            UiLayout::Solid::new().size(Abs((1240.0, 381.0))).pack(),
+            UiImage2dBundle::from(assets.main_logo.clone())
         ));
 
 
