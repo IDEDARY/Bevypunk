b09b9a0c68a79e2f1f1d87eb79bf98c2ae629f47
IDEDARY <IDEDARY@gmail.com>
2024-02-18 15:43:42 +0100

    sync

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use boilerplate::*;
 fn main() {
     App::new()
         .add_plugins((default_plugins(), UiPlugin::<NoData, NoData, MenuUi>::new()))
-        .add_plugins(UiDebugPlugin::<NoData, NoData, MenuUi>::new())
+        //.add_plugins(UiDebugPlugin::<NoData, NoData, MenuUi>::new())
         .add_plugins(VFXPlugin)
         .add_systems(PreStartup, presetup)
         .add_systems(Startup, setup)
@@ -95,7 +95,7 @@ fn setup(mut commands: Commands, assets: Res<AssetCache>) {
             ));
     
             // Spawn button text
-            /* ui.spawn((
+            ui.spawn((
                 MenuUi,
                 list.add(format!("{button}/Text")),
                 UiLayout::Solid::new().align_x(-1.0).pack(),
@@ -108,113 +108,10 @@ fn setup(mut commands: Commands, assets: Res<AssetCache>) {
                         }),
                     ..default()
                 }
-            )); */
+            ));
 
             offset += gap + size;
         }
-
-
-        ui.spawn((
-            UiLink::path("Root/Secondary"),
-            UiLayout::Window::new().pos(Prc((50.0, 0.0))).size(Prc((50.0, 50.0))).pack(),
-            UiTreeBundle::<NoData, NoData, MenuUi>::from(UiTree::new("Bevypunk2")),
-            //MovableByCamera,
-        )).with_children(|ui| {
-    
-            // Spawn the root div
-            let root = UiLink::path("Root");
-            ui.spawn((
-                MenuUi,
-                root.clone(),
-                UiLayout::Window::FULL.pack(),
-            ));
-    
-            // Spawn the background
-            ui.spawn((
-                MenuUi,
-                root.add("Background"),
-                UiLayout::Solid::new().size((2968.0, 1656.0)).cover(Cover::Full).pack(),
-                UiImage2dBundle::from(assets.main_background.clone())
-            ));
-    
-    
-            // Spawn the board
-            let board = root.add("Board");
-            ui.spawn((
-                MenuUi,
-                board.clone(),
-                UiLayout::Solid::new().size((896.0, 1656.0)).align_x(Align(-0.74)).pack(),
-                UiImage2dBundle::from(assets.main_board.clone())
-            ));
-    
-            // Spawn the logo
-            ui.spawn((
-                MenuUi,
-                board.add("Boundary"),
-                UiLayout::Window::new().pos(Prc((0.0, 13.0))).size(Prc((105.0, 20.0))).pack(),
-            ));
-            ui.spawn((
-                MenuUi,
-                board.add("Boundary/Logo"),
-                UiLayout::Solid::new().size(Abs((1240.0, 381.0))).pack(),
-                UiImage2dBundle::from(assets.main_logo.clone())
-            ));
-    
-            // Spawn button boundary
-            let list = board.add("List");
-            ui.spawn((
-                MenuUi,
-                list.clone(),
-                UiLayout::Window::new().pos(Prc((22.0, 41.0))).size(Prc((55.0, 35.0))).pack(),
-            ));
-    
-            // Spawn buttons
-            let gap = 3.0;
-            let size = 14.0;
-            let mut offset = 0.0;
-            for button in ["CONTINUE", "LOAD GAME", "SETTINGS", "CREDITS", "QUIT GAME"] {
-    
-                // Spawn button image
-                ui.spawn((
-                    MenuUi,
-                    list.add(button),
-                    UiLayout::Window::new().pos(Prc((0.0, offset))).size(Prc((100.0, size))).pack(),
-                    UiImage2dBundle {
-                        texture: assets.button.clone(),
-                        sprite: Sprite { color: Color::BEVYPUNK_RED_DIM, ..default() },
-                        ..default()
-                    },
-                    ImageScaleMode::Sliced(TextureSlicer { border: BorderRect::square(32.0), ..default() }),
-                ));
-        
-                // Spawn button text
-                /* ui.spawn((
-                    MenuUi,
-                    list.add(format!("{button}/Text")),
-                    UiLayout::Solid::new().align_x(-1.0).pack(),
-                    UiText2dBundle {
-                        text: Text::from_section(button,
-                            TextStyle {
-                                font: assets.font_medium.clone(),
-                                font_size: 32.0,
-                                color: Color::BEVYPUNK_RED,
-                            }),
-                        ..default()
-                    }
-                )); */
-    
-                offset += gap + size;
-            }
-    
-    
-            ui.spawn((
-                MenuUi,
-                UiLink::path("Root/Secondary"),
-                UiLayout::Window::new().pos(Prc((50.0, 0.0))).size(Prc((50.0, 100.0))).pack(),
-            ));
-    
-        });
-
     });
 
 }
