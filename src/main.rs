494964cbb527f2e62322344251a6c7747a8a9a6a
IDEDARY <IDEDARY@gmail.com>
2024-07-20 15:44:19 +0200

    Changed to cursor bundle

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn main() {
 
     #[cfg(not(target_family = "wasm"))]
     if let Ok(intro) = AnimatedImageLoader::load_now_from_bytes(include_bytes!("../assets/images/intro/intro.gif"), "gif", app){
-        app.insert_resource(PreLoader { intro }); 
+        app.insert_resource(PreLoader { intro });
     }
 
     app.run();
@@ -53,49 +53,45 @@ fn setup(mut commands: Commands, assets: Res<AssetServer>, mut atlas_layout: Res
 
         // Spawn cursor
         camera.spawn ((
-
-            // Here we can map different native cursor icons to texture atlas indexes and sprite offsets
-            Cursor2d::new()
-                .set_index(CursorIcon::Default, 0, (14.0, 14.0))
-                .set_index(CursorIcon::Pointer, 1, (10.0, 12.0))
-                .set_index(CursorIcon::Grab, 2, (40.0, 40.0)),
-
-            // Here we specify that the cursor should be controlled by gamepad 0
-            GamepadCursor::new(0),
-
-            // This is required for picking to work
-            PointerBundle::new(PointerId::Custom(pointer::Uuid::new_v4())),
-            
-            // Add texture atlas to the cursor
-            TextureAtlas {
-                layout: atlas_layout.add(TextureAtlasLayout::from_grid(UVec2::splat(80), 3, 1, None, None)),
-                index: 0,
-            },
-            SpriteBundle {
-                texture: assets.load(PreLoader::CURSOR),
-                transform: Transform { scale: Vec3::new(0.45, 0.45, 1.0), ..default() },
-                sprite: Sprite {
-                    color: Color::BEVYPUNK_YELLOW.with_alpha(2.0),
-                    anchor: Anchor::TopLeft,
+            StyledCursorBundle {
+                // Here we can map different native cursor icons to texture atlas indexes and sprite offsets
+                cursor: Cursor2d::new()
+                    .set_index(CursorIcon::Default, 0, (14.0, 14.0))
+                    .set_index(CursorIcon::Pointer, 1, (10.0, 12.0))
+                    .set_index(CursorIcon::Grab, 2, (40.0, 40.0)),
+                // Add texture atlas to the cursor
+                atlas: TextureAtlas {
+                    layout: atlas_layout.add(TextureAtlasLayout::from_grid(UVec2::splat(80), 3, 1, None, None)),
+                    index: 0,
+                },
+                // Add sprite strip to the cursor
+                sprite: SpriteBundle {
+                    texture: assets.load(PreLoader::CURSOR),
+                    transform: Transform { scale: Vec3::new(0.45, 0.45, 1.0), ..default() },
+                    sprite: Sprite {
+                        color: Color::BEVYPUNK_YELLOW.with_alpha(2.0),
+                        anchor: Anchor::TopLeft,
+                        ..default()
+                    },
                     ..default()
                 },
                 ..default()
             },
 
-            // Make the raycaster ignore this entity, we don't want our cursor to block clicking
-            Pickable::IGNORE,
+            // Here we specify that the cursor should be controlled by gamepad 0
+            //GamepadCursor::new(0),
         ));
     });
 
     #[cfg(not(target_family = "wasm"))]
-    {   
+    {
         // Spawn intro route
         commands.spawn(MainMenuRoute);
     }
-    
+
 
     #[cfg(target_family = "wasm")]
-    {   
+    {
         // Skip intro on wasm
         commands.spawn(MainMenuRoute);
 
